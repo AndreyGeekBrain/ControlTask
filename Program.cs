9769153541e992e993418151716f40923a3ab100
”Andrey <Soda14@mail.ru>
2023-10-05 23:51:19 +0300

    Upload_Prrogram

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿Console.WriteLine("Введите размер массива строк!");
+int sizeArray = Convert.ToInt32(Console.ReadLine());
+
+string[] сreatinArray(int sizeNumber){
+string[] strArray = new string[sizeNumber];
+Console.WriteLine("Введите массив строк,через \"Enter\".\n");
+for (int i = 0; i < strArray.Length; i++){
+string str = Console.ReadLine();
+strArray[i] = str;
+}
+return strArray;
+}
+
+string[] newCreatinArray(string[] olgArray){
+string[] newStrArray = new string[olgArray.Length];
+for (int i = 0; i < olgArray.Length; i++)
+    if(olgArray[i].Length <= 3){
+        newStrArray[i]= olgArray[i];
+}
+return newStrArray.Where(x => x != null).ToArray();
+}
+
+void arrayOutput(string[] arrayStr){
+    Console.WriteLine("\nНовый массив строк!\n");
+for (int i = 0; i < arrayStr.Length; i++)
+{
+    Console.WriteLine(arrayStr[i]);
+}
+}
+
+arrayOutput(newCreatinArray(сreatinArray(sizeArray)));
+
